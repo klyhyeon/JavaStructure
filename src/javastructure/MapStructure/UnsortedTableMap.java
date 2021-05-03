3c7030e835937b85148174b60e898c3f74614cd4
DESKTOP-3Q9NPEM\klyhy <klyhyeon@gmail.com>
2021-05-03 20:52:01 +0900

    Package, Location Adjustments

--- src/javastructure/MapStructure/UnsortedTableMap.java
+++ src/javastructure/MapStructure/UnsortedTableMap.java
@@ -1,2 +1,70 @@
-package javastructure.MapStructure;public class UnsortedTableMap {
+package javastructure.MapStructure;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NoSuchElementException;
+
+public class UnsortedTableMap<K,V> extends AbstractMap<K,V> {
+    private ArrayList<MapEntry<K,V>> table = new ArrayList<>();
+
+    public UnsortedTableMap() {}
+
+    private int findIndex(K key) {
+        int n = table.size();
+        for (int j = 0; j < n; j++)
+            if (table.get(j).getKey().equals(key))
+                return j;
+            return -1;
+    }
+
+    @Override
+    public int size() { return table.size(); }
+
+    @Override
+    public V get(K key) {
+        int j = findIndex(key);
+        if (j == -1) return null;
+        return table.get(j).getValue();
+    }
+
+    @Override
+    public V put(K key, V value) {
+        int j = findIndex(key);
+        if (j == -1) {
+            table.add(new MapEntry<>(key, value));
+            return null;
+        } else
+            return table.get(j).setValue(value);
+    }
+
+    @Override
+    public V remove(K key) {
+        int j = findIndex(key);
+        if (j == -1) return null;
+        V old = table.get(j).getValue();
+        int n = size();
+        if (j != n - 1) {
+            table.set(j, table.get(n-1));
+        }
+        table.remove(n-1);
+        return old;
+    }
+
+    private class EntryIterator implements Iterator<Map.Entry<K,V>> {
+        private int j = 0;
+        public boolean hasNext() {return j < table.size();}
+        public Map.Entry<K,V> next() {
+            if (j == table.size()) throw new NoSuchElementException();
+            return table.get(j++);
+        }
+        public void remove() {throw new UnsupportedOperationException();}
+    }
+
+    private class EntryIterable implements Iterable<Entry<K,V>> {
+        public Iterator<Entry<K,V>> iterator() {return new EntryIterator();}
+    }
+
+    public Iterable<Entry<K,V>> entrySet() {return new EntryIterable();}
 }
--- src/javastructure/MapStructure/WordCount.java
+++ src/javastructure/MapStructure/WordCount.java
@@ -1,2 +1,33 @@
-package javastructure.MapStructure;public class WordCount {
+package javastructure.MapStructure;
+
+import java.util.*;
+import java.util.Map;
+import java.util.Map.Entry;
+
+public class WordCount {
+
+    public static void main(String args[]) {
+        Map<String, Integer> freq = new LinkedHashMap<>();
+        Scanner doc = new Scanner(System.in).useDelimiter("[^a-zA-Z]+");
+        while(doc.hasNext()) {
+            String word = doc.next().toLowerCase();
+            if (word.equals("end"))
+                break;
+            if (freq.containsKey(word)) {
+                int count = freq.get(word);
+                freq.put(word, ++count);
+            } else
+                freq.put(word, 1);
+        }
+        int maxCount = 0;
+        String maxWord = "";
+        for (Entry<String, Integer> ent : freq.entrySet()) {
+            if (ent.getValue() > maxCount) {
+                maxCount = ent.getValue();
+                maxWord = ent.getKey();
+            }
+        }
+        System.out.println("The most word : " + maxWord);
+        System.out.println("The maxCount is " + maxCount);
+    }
 }
